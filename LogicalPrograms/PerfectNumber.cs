1bf7de164735139301224bd12a386464eec149fd
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-17 22:38:05 +0530

    added. a program to check a number is perfect or not

---
+++ LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class PerfectNumber
+    {
+        public static void CheckPerfectNumber()
+        {
+            Console.WriteLine("Enter the number :");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int index = 1; index <= number / 2; index++)
+            {
+                if (number % index == 0)
+                {
+                    sum = sum + index;
+                }
+            }
+            if (sum == number)
+                Console.WriteLine("Number is PerfectNumber :");
+            else
+                Console.WriteLine("Number not a perfect Number :");
+
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -6,7 +6,7 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Press a number : \nFibonacciSeries :1");
+            Console.WriteLine("Press a number : \nFibonacciSeries :1\nPerfectNumber :2");
             int problemNumber = Convert.ToInt32(Console.ReadLine());
 
             switch (problemNumber)
@@ -14,8 +14,11 @@ namespace LogicalPrograms
                 case 1:
                     FibonacciSeries.ComputingFibnacciSeries();
                     break;
+                case 2:
+                    PerfectNumber.CheckPerfectNumber();
+                    break;
                 default:
-                    Console.WriteLine( "Invalid choice");
+                    Console.WriteLine("Invalid choice");
                     break;
 
             }
