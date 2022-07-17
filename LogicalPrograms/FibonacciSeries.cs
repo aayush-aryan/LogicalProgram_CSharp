db4aeddeec264e8c9148cb446880e114dcdfac27
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-17 22:09:48 +0530

    added. program to display fibonacchi series

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class FibonacciSeries
+    {
+        public static void ComputingFibnacciSeries()
+        {
+            int firstTerm = 0, secondTerm = 1, thirdTerm;
+
+            Console.WriteLine("Enter the number of term you want FibonacciSeries : ");
+            int fibTerm = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("fibonacciSeries are :" + firstTerm + "," + secondTerm);
+            for (int index = 3; index <= fibTerm; index++)
+            {
+                thirdTerm = firstTerm + secondTerm;
+                Console.Write(","+thirdTerm);
+                firstTerm = secondTerm;
+                secondTerm = thirdTerm;
+            }
+
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press a number : \nFibonacciSeries :1");
+            int problemNumber = Convert.ToInt32(Console.ReadLine());
+
+            switch (problemNumber)
+            {
+                case 1:
+                    FibonacciSeries.ComputingFibnacciSeries();
+                    break;
+                default:
+                    Console.WriteLine( "Invalid choice");
+                    break;
+
+            }
+
+        }
+    }
+}
